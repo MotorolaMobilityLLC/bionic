spsforks/android-bionic-libc

Fixed support for RTLD_NEXT in dlsym()
The previous implementation of this flag was broken--it behaved identically
to RTLD_DEFAULT.  This adds a proper implementation, which examines the address
of the calling function, and uses it to determine which library to use to begin
the symbol search process.

linker/linker.h
@@ -224,6 +224,8 @@ soinfo *find_library(const char *name);
 unsigned unload_library(soinfo *si);
 Elf32_Sym *lookup_in_library(soinfo *si, const char *name);
 Elf32_Sym *lookup(const char *name, soinfo **found);
+Elf32_Sym *lookup_next(const char *name, soinfo **found, soinfo *si);
+soinfo *find_containing_library(unsigned addr);
 const char *linker_get_error(void);
 
 #ifdef ANDROID_ARM_LINKER 

MotorolaMobilityLLC/bionic

Change-Id: I2ad2b46363f68932af63a3828a22f9c7987eea67

linker/linker.h
@@ -224,7 +224,7 @@ extern soinfo libdl_info;
 soinfo *find_library(const char *name);
 unsigned unload_library(soinfo *si);
 Elf32_Sym *lookup_in_library(soinfo *si, const char *name);
-Elf32_Sym *lookup(const char *name, soinfo **found);
+Elf32_Sym *lookup(const char *name, soinfo **found, soinfo *start);
 soinfo *find_containing_library(void *addr);
 Elf32_Sym *find_containing_symbol(void *addr, soinfo *si);
 const char *linker_get_error(void);