77123384e62a8afca60e8405f2239f2e669d4129
Lucas Crowthers <lucasc@codeaurora.org>
2012-08-27 14:21:19 -0400

    Bionic/libm: add assembly versions of sin/cos.
    Add assembly versions of sin/cos with integrated remainder pi/2
    calculation.  Directly extracted from binary libm.so compiled with
    __ieee754_rem_pio2 calls inlined.
    
    Change-Id: Ia093f420e58e794635e3a5f09e8236ae7601f1f6
    Reviewed-on: http://gerrit.pcs.mot.com/568829
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Ye Ouyang <a5111c@motorola.com>
    SLTApproved: Ye Ouyang <a5111c@motorola.com>
    Submit-Approved: Ye Ouyang <a5111c@motorola.com>
    Tested-by: Ye Ouyang <a5111c@motorola.com>

--- libm/Android.mk
+++ libm/Android.mk
@@ -93,7 +93,6 @@ libm_common_src_files += \
     upstream-freebsd/lib/msun/src/s_conjf.c \
     upstream-freebsd/lib/msun/src/s_copysign.c \
     upstream-freebsd/lib/msun/src/s_copysignf.c \
-    upstream-freebsd/lib/msun/src/s_cos.c \
     upstream-freebsd/lib/msun/src/s_cosf.c \
     upstream-freebsd/lib/msun/src/s_cproj.c \
     upstream-freebsd/lib/msun/src/s_cprojf.c \
@@ -163,7 +162,6 @@ libm_common_src_files += \
     upstream-freebsd/lib/msun/src/s_signgam.c \
     upstream-freebsd/lib/msun/src/s_significand.c \
     upstream-freebsd/lib/msun/src/s_significandf.c \
-    upstream-freebsd/lib/msun/src/s_sin.c \
     upstream-freebsd/lib/msun/src/s_sinf.c \
     upstream-freebsd/lib/msun/src/s_tan.c \
     upstream-freebsd/lib/msun/src/s_tanf.c \
@@ -178,6 +176,18 @@ libm_common_src_files += \
     upstream-freebsd/lib/msun/src/w_dremf.c \
 
 libm_common_src_files += fake_long_double.c
+
+  ifeq ($(TARGET_CPU_VARIANT),krait)
+    libm_common_src_files += \
+	  arm/e_pow.S	\
+	  arm/s_cos.S	\
+	  arm/s_sin.S
+    libm_common_cflags += -DKRAIT_NEON_OPTIMIZATION -fno-if-conversion
+  else
+    libm_common_src_files += \
+	  upstream-freebsd/lib/msun/src/s_cos.c \
+	  upstream-freebsd/lib/msun/src/s_sin.c
+  endif
 
 # TODO: on Android, "long double" is "double".
 #    upstream-freebsd/lib/msun/src/e_acosl.c \
