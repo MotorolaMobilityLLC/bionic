f1e5d94ccee94428e026da592870a963bd522551
Brent DeGraaf <bdegraaf@codeaurora.org>
2013-10-03 16:37:39 -0400

    libc: arm: Allow optional optimization of memmove and bcopy
    The libc functions memmove and bcopy are not configurable target-by-
    target.  This change allows per-target optimization for best perfor-
    mance.
    
    Change-Id: Id684672fbf0bcffbe68c36a28b64186a6c2554f3

--- libc/arch-arm/generic/generic.mk
+++ libc/arch-arm/generic/generic.mk
@@ -1,5 +1,7 @@
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/generic/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/generic/bionic/memset.S)
+$(call libc-add-cpu-variant-src,MEMMOVE,bionic/memmove.c.arm)
+$(call libc-add-cpu-variant-src,BCOPY,string/bcopy.c.arm)
 $(call libc-add-cpu-variant-src,STRCAT,string/strcat.c)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/generic/bionic/strcmp.S)
 $(call libc-add-cpu-variant-src,STRCPY,arch-arm/generic/bionic/strcpy.S)
