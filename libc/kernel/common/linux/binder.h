3397d9f41ce8cbd338545c745326fb18a36843ff
Jing Ji <a5705c@motorola.com>
2013-10-28 13:15:38 -0500

    IKXREL1KK-3220: Scorpion_mini:UT: System Server Watchdog
    PROPAGATE_FROM: IKJBREL1-3965
    
    The system_server was blocked at a binder call into app process
    for a long duration and got a watchdog reset.
    
    In order to prevent this from happening, this patch implements:
    * Check whether there is an outgoing binder call from
    system_server is stuck or not, return remote process id if any.
    * Dump the stack trace of remote process of the stuck binder
    transaction.
    * Kill remote process of the stuck binder transaction prior to
    watchdog reset, thus system_server could recover from it.
    
    Signed-off-by: Jing Ji <a5705c@motorola.com>
    Change-Id: I61dc01fe4dfa372a6ae6278dc201935d9f3adfcd
    Reviewed-on: http://gerrit.pcs.mot.com/578211
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Libin Tang <Libin.Tang@motorola.com>
    SLTApproved: Libin Tang <Libin.Tang@motorola.com>
    Reviewed-by: Xin Guan <a18772@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- libc/kernel/common/linux/binder.h
+++ libc/kernel/common/linux/binder.h
@@ -61,6 +61,11 @@ struct binder_version {
  signed long protocol_version;
 };
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+struct binder_get_peer {
+ pid_t self;
+ pid_t remote;
+};
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define BINDER_CURRENT_PROTOCOL_VERSION 7
 #define BINDER_WRITE_READ _IOWR('b', 1, struct binder_write_read)
 #define BINDER_SET_IDLE_TIMEOUT _IOW('b', 3, int64_t)
@@ -71,101 +76,103 @@ struct binder_version {
 #define BINDER_THREAD_EXIT _IOW('b', 8, int)
 #define BINDER_VERSION _IOWR('b', 9, struct binder_version)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define BINDER_GET_PEER_PID _IOWR('b', 10, struct binder_get_peer)
 enum transaction_flags {
  TF_ONE_WAY = 0x01,
  TF_ROOT_OBJECT = 0x04,
- TF_STATUS_CODE = 0x08,
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ TF_STATUS_CODE = 0x08,
  TF_ACCEPT_FDS = 0x10,
 };
 struct binder_transaction_data {
- union {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ union {
  size_t handle;
  void *ptr;
  } target;
- void *cookie;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ void *cookie;
  unsigned int code;
  unsigned int flags;
  pid_t sender_pid;
- uid_t sender_euid;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ uid_t sender_euid;
  size_t data_size;
  size_t offsets_size;
  union {
- struct {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ struct {
  const void *buffer;
  const void *offsets;
  } ptr;
- uint8_t buf[8];
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ uint8_t buf[8];
  } data;
 };
 struct binder_ptr_cookie {
- void *ptr;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ void *ptr;
  void *cookie;
 };
 struct binder_pri_desc {
- int priority;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ int priority;
  int desc;
 };
 struct binder_pri_ptr_cookie {
- int priority;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ int priority;
  void *ptr;
  void *cookie;
 };
-enum BinderDriverReturnProtocol {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+enum BinderDriverReturnProtocol {
  BR_ERROR = _IOR_BAD('r', 0, int),
  BR_OK = _IO('r', 1),
  BR_TRANSACTION = _IOR_BAD('r', 2, struct binder_transaction_data),
- BR_REPLY = _IOR_BAD('r', 3, struct binder_transaction_data),
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BR_REPLY = _IOR_BAD('r', 3, struct binder_transaction_data),
  BR_ACQUIRE_RESULT = _IOR_BAD('r', 4, int),
  BR_DEAD_REPLY = _IO('r', 5),
  BR_TRANSACTION_COMPLETE = _IO('r', 6),
- BR_INCREFS = _IOR_BAD('r', 7, struct binder_ptr_cookie),
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BR_INCREFS = _IOR_BAD('r', 7, struct binder_ptr_cookie),
  BR_ACQUIRE = _IOR_BAD('r', 8, struct binder_ptr_cookie),
  BR_RELEASE = _IOR_BAD('r', 9, struct binder_ptr_cookie),
  BR_DECREFS = _IOR_BAD('r', 10, struct binder_ptr_cookie),
- BR_ATTEMPT_ACQUIRE = _IOR_BAD('r', 11, struct binder_pri_ptr_cookie),
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BR_ATTEMPT_ACQUIRE = _IOR_BAD('r', 11, struct binder_pri_ptr_cookie),
  BR_NOOP = _IO('r', 12),
  BR_SPAWN_LOOPER = _IO('r', 13),
  BR_FINISHED = _IO('r', 14),
- BR_DEAD_BINDER = _IOR_BAD('r', 15, void *),
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BR_DEAD_BINDER = _IOR_BAD('r', 15, void *),
  BR_CLEAR_DEATH_NOTIFICATION_DONE = _IOR_BAD('r', 16, void *),
  BR_FAILED_REPLY = _IO('r', 17),
 };
-enum BinderDriverCommandProtocol {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+enum BinderDriverCommandProtocol {
  BC_TRANSACTION = _IOW_BAD('c', 0, struct binder_transaction_data),
  BC_REPLY = _IOW_BAD('c', 1, struct binder_transaction_data),
  BC_ACQUIRE_RESULT = _IOW_BAD('c', 2, int),
- BC_FREE_BUFFER = _IOW_BAD('c', 3, int),
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BC_FREE_BUFFER = _IOW_BAD('c', 3, int),
  BC_INCREFS = _IOW_BAD('c', 4, int),
  BC_ACQUIRE = _IOW_BAD('c', 5, int),
  BC_RELEASE = _IOW_BAD('c', 6, int),
- BC_DECREFS = _IOW_BAD('c', 7, int),
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BC_DECREFS = _IOW_BAD('c', 7, int),
  BC_INCREFS_DONE = _IOW_BAD('c', 8, struct binder_ptr_cookie),
  BC_ACQUIRE_DONE = _IOW_BAD('c', 9, struct binder_ptr_cookie),
  BC_ATTEMPT_ACQUIRE = _IOW_BAD('c', 10, struct binder_pri_desc),
- BC_REGISTER_LOOPER = _IO('c', 11),
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BC_REGISTER_LOOPER = _IO('c', 11),
  BC_ENTER_LOOPER = _IO('c', 12),
  BC_EXIT_LOOPER = _IO('c', 13),
  BC_REQUEST_DEATH_NOTIFICATION = _IOW_BAD('c', 14, struct binder_ptr_cookie),
- BC_CLEAR_DEATH_NOTIFICATION = _IOW_BAD('c', 15, struct binder_ptr_cookie),
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ BC_CLEAR_DEATH_NOTIFICATION = _IOW_BAD('c', 15, struct binder_ptr_cookie),
  BC_DEAD_BINDER_DONE = _IOW_BAD('c', 16, void *),
 };
 #endif
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
