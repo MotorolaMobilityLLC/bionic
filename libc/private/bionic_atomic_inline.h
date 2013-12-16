spsforks/android-bionic-libc

AArch64: Add support for AArch64 atomic operations
This patch adds support for AArch64 atomic operations. Some
of the stubs use the lightweight store/load exclusive.

Change-Id: Iaf704d048b2dc15bf08cf8e4f0c3ea9f2052fe13
Signed-off-by: Serban Constantinescu <serban.constantinescu@arm.com>

libc/private/bionic_atomic_inline.h
@@ -55,6 +55,8 @@ extern "C" {
 #  include "bionic_atomic_x86.h"
 #elif defined(__mips__)
 #  include "bionic_atomic_mips.h"
+#elif defined(__aarch64__)
+#  include "bionic_atomic_aarch64.h"
 #else
 #  include "bionic_atomic_gcc_builtin.h"
 #endif

MotorolaMobilityLLC/bionic

libc/private/bionic_atomic_inline.h
@@ -44,7 +44,9 @@ extern "C" {
  */
 #define  __ATOMIC_INLINE__  static __inline__ __attribute__((always_inline))
 
-#ifdef __arm__
+#if defined(__aarch64__)
+#  include "bionic_atomic_aarch64.h"
+#elif defined(__arm__)
 #  include "bionic_atomic_arm.h"
 #elif defined(__i386__)
 #  include "bionic_atomic_x86.h"