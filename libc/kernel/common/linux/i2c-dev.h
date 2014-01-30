90d2a4e1c764e6af6cc5a77f8a78e6463b7fd0bd
Jeffrey Carlyle <jeff.carlyle@motorola.com>
2014-01-30 16:18:45 -0600

    include: add i2c-dev.h
    Change-Id: Ic75e5d0c8293e1a1f43a005a88099ae41b2bd4c8
    Signed-off-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/607324
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jing Ji <a5705c@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ libc/kernel/common/linux/i2c-dev.h
@@ -0,0 +1,25 @@
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ***   To edit the content of this header, modify the corresponding
+ ***   source file (e.g. under external/kernel-headers/original/) then
+ ***   run bionic/libc/kernel/tools/update_all.py
+ ***
+ ***   Any manual change here will be lost the next time this script will
+ ***   be run. You've been warned!
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef _LINUX_I2C_DEV_H
+#define _LINUX_I2C_DEV_H
+#include <uapi/linux/i2c-dev.h>
+#define I2C_MAJOR 89
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#endif
+
