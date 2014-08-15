e02e46a6f3b4456b1acc2d8b1c8bc715c6497347
Xin Qi <xqi@codeaurora.org>
2014-08-15 11:12:32 -0700

    Performance: krait: Implement optimized versions of memmove
    Code has been refactored to thumb2 for consistency with the rest
    of bionic libc, as well as performance and correctness.
    
    Change-Id: I5f738ef3eb12ece6b55285f1588eab3d4bbbe27d
    Reviewed-on: /783632
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jing Ji <a5705c@motorola.com>
    Reviewed-by: Xin Guan <a18772@motorola.com>
    SLTApproved: Xin Guan <a18772@motorola.com>

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -4,6 +4,7 @@ libc_bionic_src_files_arm += \
     arch-arm/krait/bionic/strcmp.S \
     arch-arm/krait/bionic/__strcat_chk.S \
     arch-arm/krait/bionic/__strcpy_chk.S \
+    arch-arm/krait/bionic/memmove.S
 
 # Use cortex-a15 versions of strcat/strcpy/strlen and standard memmove
 libc_bionic_src_files_arm += \
@@ -15,5 +16,4 @@ libc_bionic_src_files_arm += \
 libc_bionic_src_files_arm += \
     arch-arm/generic/bionic/memcmp.S \
 
-libc_bionic_src_files_arm += \
-    arch-arm/denver/bionic/memmove.S \
+
