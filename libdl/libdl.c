spsforks/android-bionic-libc

Add functions to provide target sdk version
Bug: http://b/21364029
Change-Id: I8648d1bff6c8fd6e7cd12da7f128e048b9f2829a

libdl/libdl.c
@@ -17,6 +17,7 @@
 #include <dlfcn.h>
 #include <link.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <android/dlext.h>
 
 // These are stubs for functions that are actually defined
@@ -38,3 +39,6 @@ void android_get_LD_LIBRARY_PATH(char* buffer __unused, size_t buffer_size __unu
 void android_update_LD_LIBRARY_PATH(const char* ld_library_path __unused) { }
 
 void* android_dlopen_ext(const char* filename __unused, int flag __unused, const android_dlextinfo* extinfo __unused) { return 0; }
+
+void _dl_android_set_application_target_sdk_version(uint32_t target __unused) { }
+uint32_t _dl_android_get_application_target_sdk_version() { return 0; }

tests/Android.mk
@@ -49,6 +49,9 @@ endif
 test_cppflags = \
     -std=gnu++11 \
 
+libBionicStandardTests_src_files_target := \
+    appsdk_test.cpp \
+
 libBionicStandardTests_src_files := \
     arpa_inet_test.cpp \
     buffer_tests.cpp \

MotorolaMobilityLLC/bionic

(cherry picked from commit 79fd668bb4ddb22432eeda2ebd8d10359013d9a8)

libdl/libdl.c
@@ -17,6 +17,7 @@
 #include <dlfcn.h>
 #include <link.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <android/dlext.h>
 
 // These are stubs for functions that are actually defined
@@ -38,3 +39,6 @@ void android_get_LD_LIBRARY_PATH(char* buffer __unused, size_t buffer_size __unu
 void android_update_LD_LIBRARY_PATH(const char* ld_library_path __unused) { }
 
 void* android_dlopen_ext(const char* filename __unused, int flag __unused, const android_dlextinfo* extinfo __unused) { return 0; }
+
+void android_set_application_target_sdk_version(uint32_t target __unused) { }
+uint32_t android_get_application_target_sdk_version() { return 0; }

tests/Android.mk
@@ -49,6 +49,9 @@ endif
 test_cppflags = \
     -std=gnu++11 \
 
+libBionicStandardTests_src_files_target := \
+    libdl_test.cpp \
+
 libBionicStandardTests_src_files := \
     arpa_inet_test.cpp \
     buffer_tests.cpp \