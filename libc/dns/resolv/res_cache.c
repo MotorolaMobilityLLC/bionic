be59306c47f3b2fe1e88c5958ccf7c10ccb37c9d
Weizhong Xia <w20079@motorola.com>
2015-07-01 15:40:05 -0500

    rework dns retry mechanism for wifi link
    Use new retry mechanism for wifi link
    Here is the new mechanism:
    1. We do dns query in two rounds, each round with total timeout value 5 seconds
    2. In each round if we did not get response from a server within 100 ms, start
       to try next server. If response comes in at a later time, it will still be accepted
    3. Anytime we received a response, dns query stopped successfully
    4. Total timeout value is 10 seconds, this can still tolerate some slow server/link
    
    Change-Id: I688f0e3f51b3e2b41dd62d4e2c1bbf3a127591b3
    Reviewed-on: /762726
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Fu-Xiang Long <fgcp188@motorola.com>
    Reviewed-by: De-Yu Wu <dbk378@motorola.com>
    Reviewed-by: Jing Ji <a5705c@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: /764629
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Wei-Zhong Xia <w20079@motorola.com>

--- libc/dns/resolv/res_cache.c
+++ libc/dns/resolv/res_cache.c
@@ -1241,6 +1241,7 @@ struct resolv_cache_info {
     struct __res_stats          nsstats[MAXNS];
     char                        defdname[MAXDNSRCHPATH];
     int                         dnsrch_offset[MAXDNSRCH+1];  // offsets into defdname
+    int                         isWifi; // IKSWL-25609 rework dns retry mechanism
 };
 
 #define  HTABLE_VALID(x)  ((x) != NULL && (x) != HTABLE_DELETED)
@@ -1956,6 +1957,10 @@ _resolv_set_nameservers_for_net(unsigned netid, const char** servers, unsigned n
     register char *cp;
     int *offset;
     struct addrinfo* nsaddrinfo[MAXNS];
+    // BEGIN MOTO IKSWL-25609 rework dns retry mechanism
+    const char * NAME = "net.wifi.netid";
+    char value[PROP_VALUE_MAX];
+    // END MOTO
 
     if (numservers > MAXNS) {
         XLOG("%s: numservers=%u, MAXNS=%u", __FUNCTION__, numservers, MAXNS);
@@ -1998,6 +2003,14 @@ _resolv_set_nameservers_for_net(unsigned netid, const char** servers, unsigned n
         } else {
             _resolv_set_default_params(&cache_info->params);
         }
+        // BEGIN MOTO IKSWL-25609 rework dns retry mechanism
+        __system_property_get(NAME, value);
+        if (atoi(value) == (int)netid) {
+            cache_info->isWifi = 1;
+        } else {
+            cache_info->isWifi = 0;
+        }
+        // END MOTO
 
         if (!_resolv_is_nameservers_equal_locked(cache_info, servers, numservers)) {
             // free current before adding new
@@ -2122,6 +2135,7 @@ _resolv_populate_res_for_net(res_state statp)
         int nserv;
         struct addrinfo* ai;
         XLOG("%s: %u\n", __FUNCTION__, statp->netid);
+        if (info->isWifi) statp->options |= RES_QUICKRETRY; // IKSWL-25609 rework dns retry mechanism
         for (nserv = 0; nserv < MAXNS; nserv++) {
             ai = info->nsaddrinfo[nserv];
             if (ai == NULL) {
