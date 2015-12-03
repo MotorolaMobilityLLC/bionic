67dbf0a09fd2897a4645b40ffae90685e4c243b3
Ling-Yan Ren <e5949c@motorola.com>
2015-12-03 17:30:37 -0600

    Revert "(CR): allow cci i2c frequency mode selection by userspace"
    This reverts commit dac406930f5d7b057831c743ee416559a34aa5ec.
    
    Change-Id: I44e233c4dde83aa3e435b916657d63ec02f059fc
    Reviewed-on: /804830
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ling-Yan Ren <e5949c@motorola.com>
    Submit-Approved: Ling-Yan Ren <e5949c@motorola.com>
    Tested-by: Ling-Yan Ren <e5949c@motorola.com>
    SLTApproved: Yu Liu <e7664c@motorola.com>

--- libc/kernel/common/linux/cci_intf.h
+++ libc/kernel/common/linux/cci_intf.h
@@ -21,31 +21,27 @@
 #include <linux/ioctl.h>
 #include <linux/videodev2.h>
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#include <media/msm_camsensor_sdk.h>
 #define MSM_CCI_INTF_MAX_XFER 256
 struct msm_cci_intf_register {
-  unsigned short width;
+ unsigned short width;
+ unsigned short addr;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-  unsigned short addr;
 };
 struct msm_cci_intf_data {
-  unsigned short count;
+ unsigned short count;
+ unsigned char buf[MSM_CCI_INTF_MAX_XFER];
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-  unsigned char buf[MSM_CCI_INTF_MAX_XFER];
 };
 struct msm_cci_intf_xfer {
-  unsigned short cci_bus;
+ unsigned short cci_bus;
+ unsigned short slave_addr;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-  unsigned short slave_addr;
-  enum i2c_freq_mode_t i2c_freq_mode;
-  struct msm_cci_intf_register reg;
-  struct msm_cci_intf_data data;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ struct msm_cci_intf_register reg;
+ struct msm_cci_intf_data data;
 };
-#define MSM_CCI_INTF_READ _IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
-#define MSM_CCI_INTF_READ32 _IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
-#define MSM_CCI_INTF_WRITE _IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_READ   _IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define MSM_CCI_INTF_WRITE32 _IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_WRITE   _IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_READ32   _IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_WRITE32   _IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
 #endif
-
