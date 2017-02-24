spsforks/android-bionic-libc

Use <linux/eventpoll.h> to implement <sys/epoll.h>.
Bug: https://github.com/android-ndk/ndk/issues/302#issuecomment-281798384
Test: builds
Change-Id: Ia3074326a128c38f2488e342c028cc030801cfd9

libc/include/sys/epoll.h
@@ -34,6 +34,10 @@
 #include <fcntl.h> /* For O_CLOEXEC. */
 #include <signal.h> /* For sigset_t. */
 
+#define epoll_event __kernel_epoll_event
+#include <linux/eventpoll.h>
+#undef epoll_event
+
 __BEGIN_DECLS
 
 #define EPOLLIN          0x00000001
@@ -47,15 +51,6 @@ __BEGIN_DECLS
 #define EPOLLWRBAND      0x00000200
 #define EPOLLMSG         0x00000400
 #define EPOLLRDHUP       0x00002000
-#define EPOLLWAKEUP      0x20000000
-#define EPOLLONESHOT     0x40000000
-#define EPOLLET          0x80000000
-
-#define EPOLL_CTL_ADD    1
-#define EPOLL_CTL_DEL    2
-#define EPOLL_CTL_MOD    3
-
-#define EPOLL_CLOEXEC O_CLOEXEC
 
 typedef union epoll_data {
   void* ptr;

MotorolaMobilityLLC/bionic

Bug: https://github.com/android-ndk/ndk/issues/302

libc/include/sys/epoll.h
@@ -31,11 +31,17 @@
 
 #include <sys/cdefs.h>
 #include <sys/types.h>
-#include <fcntl.h> /* For O_CLOEXEC. */
 #include <signal.h> /* For sigset_t. */
+
+#include <linux/eventpoll.h>
+/* TODO: https://lkml.org/lkml/2017/2/23/416 has a better fix. */
+#undef EPOLLWAKEUP
+#undef EPOLLONESHOT
+#undef EPOLLET
 
 __BEGIN_DECLS
 
+/* TODO: remove once https://lkml.org/lkml/2017/2/23/417 is upstream. */
 #define EPOLLIN          0x00000001
 #define EPOLLPRI         0x00000002
 #define EPOLLOUT         0x00000004
@@ -50,12 +56,6 @@ __BEGIN_DECLS
 #define EPOLLWAKEUP      0x20000000
 #define EPOLLONESHOT     0x40000000
 #define EPOLLET          0x80000000
-
-#define EPOLL_CTL_ADD    1
-#define EPOLL_CTL_DEL    2
-#define EPOLL_CTL_MOD    3
-
-#define EPOLL_CLOEXEC O_CLOEXEC
 
 typedef union epoll_data {
   void* ptr;