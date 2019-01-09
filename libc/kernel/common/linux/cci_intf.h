dba2ceca91da9a3541d4310ac7b40d8a8105d95f
chenglong1 <chenglong1@lenovo.com>
2019-01-09 12:04:13 +0800

    Add init/release operation for CCI interface.
    Separate the CCI init and release from read/write operation
    to avoid too frequently init/release operation.
    
    Change-Id: I483fda3ac42d4f9d5a5d0236c8e3a3cc70865790
    Signed-off-by: chenglong1 <chenglong1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1290273
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: Jing Ji <a5705c@motorola.com>
    Reviewed-by: Chuanxin Guo <guocx4@motorola.com>
    Submit-Approved: Jira Key

--- libc/kernel/common/linux/cci_intf.h
+++ libc/kernel/common/linux/cci_intf.h
@@ -20,28 +20,27 @@
 #define _CCI_INTF_H_
 #include <linux/ioctl.h>
 #include <linux/videodev2.h>
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define MSM_CCI_INTF_MAX_XFER 256
 struct msm_cci_intf_register {
- unsigned short width;
- unsigned short addr;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  unsigned short width;
+  unsigned short addr;
 };
 struct msm_cci_intf_data {
- unsigned short count;
- unsigned char buf[MSM_CCI_INTF_MAX_XFER];
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  unsigned short count;
+  unsigned char buf[MSM_CCI_INTF_MAX_XFER];
 };
 struct msm_cci_intf_xfer {
- unsigned short cci_bus;
- unsigned short slave_addr;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- struct msm_cci_intf_register reg;
- struct msm_cci_intf_data data;
+  unsigned short cci_bus;
+  unsigned short slave_addr;
+  struct msm_cci_intf_register reg;
+  struct msm_cci_intf_data data;
 };
-#define MSM_CCI_INTF_READ   _IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define MSM_CCI_INTF_WRITE   _IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
-#define MSM_CCI_INTF_READ32   _IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
-#define MSM_CCI_INTF_WRITE32   _IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_READ _IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_READ32 _IOWR('X', BASE_VIDIOC_PRIVATE + 50, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_WRITE _IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_WRITE32 _IOWR('X', BASE_VIDIOC_PRIVATE + 51, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_INIT _IOWR('X', BASE_VIDIOC_PRIVATE + 52, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_INIT32 _IOWR('X', BASE_VIDIOC_PRIVATE + 52, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_RELEASE _IOWR('X', BASE_VIDIOC_PRIVATE + 53, struct msm_cci_intf_xfer)
+#define MSM_CCI_INTF_RELEASE32 _IOWR('X', BASE_VIDIOC_PRIVATE + 53, struct msm_cci_intf_xfer)
 #endif
