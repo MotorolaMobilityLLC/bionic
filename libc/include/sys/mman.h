spsforks/android-bionic-libc

Add bionic headers for process_madvise
Change-Id: I396945b95de364055b87cc53321aed4fad4ebc70

libc/include/sys/mman.h
@@ -32,6 +32,7 @@
 #include <sys/types.h>
 #include <linux/memfd.h>
 #include <linux/mman.h>
+#include <linux/uio.h>
 
 __BEGIN_DECLS
 
@@ -160,6 +161,13 @@ int mincore(void* __addr, size_t __size, unsigned char* __vector);
  * Returns 0 on success, and returns -1 and sets `errno` on failure.
  */
 int madvise(void* __addr, size_t __size, int __advice);
+
+/**
+ * gives the kernel advice about future usage patterns of a pid.
+ *
+ * Returns 0 on success, and returns -1 and sets `errno` on failure.
+ */
+int process_madvise(int pidfd, const struct iovec* vec, size_t vlen, int behavior, unsigned int flags);
 
 #if defined(__USE_GNU)
 

MotorolaMobilityLLC/bionic

Test: atest bionic-unit-tests-static
Bug: 173258203

libc/include/sys/mman.h
@@ -32,6 +32,7 @@
 #include <sys/types.h>
 #include <linux/memfd.h>
 #include <linux/mman.h>
+#include <linux/uio.h>
 
 __BEGIN_DECLS
 
@@ -160,6 +161,15 @@ int mincore(void* __addr, size_t __size, unsigned char* __vector);
  * Returns 0 on success, and returns -1 and sets `errno` on failure.
  */
 int madvise(void* __addr, size_t __size, int __advice);
+
+/**
+ * [process_madvise(2)](http://man7.org/linux/man-pages/man2/process_madvise.2.html)
+ * works just like madvise(2) but applies to the process specified by the given
+ * PID file descriptor.
+ *
+ * Returns 0 on success, and returns -1 and sets `errno` on failure.
+ */
+int process_madvise(int __pid_fd, const struct iovec* __iov, size_t __count, int __advice, unsigned int __flags);
 
 #if defined(__USE_GNU)
 